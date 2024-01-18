e0bcb85a6105eb4fc857e9e856be634c479d5123
zwei <wei.zhong@northeastern.edu>
2024-01-17 20:52:07 -0800

    finish assignment 1

---
+++ assignment1/assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by Zhongshu Wei on 1/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
